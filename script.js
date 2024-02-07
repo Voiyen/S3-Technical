5c88b114744cbe7de8a75684e744ea8476f96447
Nikki <122008366+Voiyen@users.noreply.github.com>
2024-02-07 23:10:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var display = document.getElementById('display');
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
